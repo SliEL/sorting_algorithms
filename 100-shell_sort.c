1d8497ea0e4882fb9d1771e13f0fb96904b09193
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-09-24 12:36:13 +0100

    shell sort task

---
+++ 100-shell_sort.c
@@ -0,0 +1,51 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+
+/**
+ * shell_sort - Sorts arrays of integers in ascending
+ *              order using the shell sort algorithm.
+ * @array: Array of integers.
+ * @size: Size of the array.
+ *
+ * Description: Uses the Knuth interval sequence.
+ */
+
+void shell_sort(int *array, size_t size)
+{
+    size_t gap, i, j;
+    int temp;
+
+    while (gap < size / 3)
+        gap = gap * 3 + 1;
+    
+    while (gap > 0)
+    {
+        for (i = gap; i < size; i++)
+        {
+            temp = array[i];
+            j = i;
+            while (j >= gap && array[j - gap] > temp)
+            {
+                array[j] = array[j - gap];
+                j = j - gap;
+            }
+            array[j] = temp;
+        }
+        gap = (gap - 1) / 3;
+        print_array(array, size);
+    }
+}
--- sort.h
+++ sort.h
@@ -8,6 +8,7 @@
 #define UP 0
 #define DOWN 1
 
+
 /**
  * enum bool - Enumeration of Boolean values.
  * @false: Equals 0.
@@ -49,6 +50,9 @@ void selection_sort(int *array, size_t size);
 int lomuto_partition(int *array, size_t size, int left, int right);
 void lomuto_sort(int *array, size_t size, int left, int right);
 void quick_sort(int *array, size_t size);
+
+/* ##### shell Sort ############## */
+void shell_sort(int *array, size_t size);
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
